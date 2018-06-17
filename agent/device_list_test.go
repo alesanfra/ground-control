a98f4057a92a5abbd8f2f41b616a8804c9df1739
Alessio Sanfratello <sanfra90@gmail.com>
2018-06-17 19:31:24 +0200

    Updated unit tests to use Convey framework

--- agent/device_list_test.go
+++ agent/device_list_test.go
@@ -4,28 +4,31 @@ import (
 	"testing"
 
 	nmap "github.com/lair-framework/go-nmap"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-var expectedJSON = `{"test_mac":{"starttime":"0001-01-01 00:00:00 +0000 UTC","endtime":"0001-01-01 00:00:00 +0000 UTC","comment":"test comment","status":{"state":"","reason":"","reason_ttl":0},"addresses":[{"addr":"test_mac","addrtype":"mac","vendor":""}],"hostnames":null,"smurfs":null,"ports":null,"extraports":null,"os":{"portsused":null,"osmatches":null,"osfingerprints":null},"distance":{"value":0},"uptime":{"seconds":0,"lastboot":""},"tcpsequence":{"index":0,"difficulty":"","vaules":""},"ipidsequence":{"class":"","values":""},"tcptssequence":{"class":"","values":""},"hostscripts":null,"trace":{"proto":"","port":0,"hops":null},"times":{"srtt":"","rttv":"","to":""}}}`
-
-func TestAsJSONEmpty(t *testing.T) {
-	var testDevices DeviceList
-	testDevices.Init()
-	j := testDevices.AsJSON()
-	s := string(j)
-	if s != "{}" {
-		t.Error("Expected", "{}", "Got", s)
-	}
-}
-
 func TestAsJSON(t *testing.T) {
-	var testDevices DeviceList
-	testDevices.Init()
-	testDevices.UpdateWithDiscoveryResult([]nmap.Host{
-		{Comment: "test comment", Addresses: []nmap.Address{{Addr: "test_mac", AddrType: "mac"}}}})
-	j := testDevices.AsJSON()
-	s := string(j)
-	if s != expectedJSON {
-		t.Error("Expected", expectedJSON, "Got", s)
-	}
+	Convey("Given a device list", t, func() {
+		var testDevices DeviceList
+		testDevices.Init()
+
+		Convey("When the list is empty", func() {
+			j := string(testDevices.AsJSON())
+
+			Convey("The resulting json should be an empty object", func() {
+				So(j, ShouldEqual, "{}")
+			})
+		})
+
+		Convey("When the list contains devices", func() {
+			testDevices.UpdateWithDiscoveryResult([]nmap.Host{
+				{Comment: "test comment", Addresses: []nmap.Address{{Addr: "test_mac", AddrType: "mac"}}}})
+			j := string(testDevices.AsJSON())
+
+			Convey("The resulting json should contain devices as well", func() {
+				expectedJSON := `{"test_mac":{"starttime":"0001-01-01 00:00:00 +0000 UTC","endtime":"0001-01-01 00:00:00 +0000 UTC","comment":"test comment","status":{"state":"","reason":"","reason_ttl":0},"addresses":[{"addr":"test_mac","addrtype":"mac","vendor":""}],"hostnames":null,"smurfs":null,"ports":null,"extraports":null,"os":{"portsused":null,"osmatches":null,"osfingerprints":null},"distance":{"value":0},"uptime":{"seconds":0,"lastboot":""},"tcpsequence":{"index":0,"difficulty":"","vaules":""},"ipidsequence":{"class":"","values":""},"tcptssequence":{"class":"","values":""},"hostscripts":null,"trace":{"proto":"","port":0,"hops":null},"times":{"srtt":"","rttv":"","to":""}}}`
+				So(j, ShouldEqual, expectedJSON)
+			})
+		})
+	})
 }
