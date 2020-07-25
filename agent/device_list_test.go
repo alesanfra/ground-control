29381431c1df1560a22cf1c9ec271ecbf906e36d
Alessio Sanfratello <sanfra90@gmail.com>
2020-07-25 14:42:36 +0200

    Swicthed to go modules

--- agent/device_list_test.go
+++ agent/device_list_test.go
@@ -26,7 +26,7 @@ func TestAsJSON(t *testing.T) {
 			j := string(testDevices.AsJSON())
 
 			Convey("The resulting json should contain devices as well", func() {
-				expectedJSON := `{"test_mac":{"starttime":"0001-01-01 00:00:00 +0000 UTC","endtime":"0001-01-01 00:00:00 +0000 UTC","comment":"test comment","status":{"state":"","reason":"","reason_ttl":0},"addresses":[{"addr":"test_mac","addrtype":"mac","vendor":""}],"hostnames":null,"smurfs":null,"ports":null,"extraports":null,"os":{"portsused":null,"osmatches":null,"osfingerprints":null},"distance":{"value":0},"uptime":{"seconds":0,"lastboot":""},"tcpsequence":{"index":0,"difficulty":"","vaules":""},"ipidsequence":{"class":"","values":""},"tcptssequence":{"class":"","values":""},"hostscripts":null,"trace":{"proto":"","port":0,"hops":null},"times":{"srtt":"","rttv":"","to":""}}}`
+				expectedJSON := `{"test_mac":{"starttime":-62135596800,"endtime":-62135596800,"comment":"test comment","status":{"state":"","reason":"","reason_ttl":0},"addresses":[{"addr":"test_mac","addrtype":"mac","vendor":""}],"hostnames":null,"smurfs":null,"ports":null,"extraports":null,"os":{"portsused":null,"osmatches":null,"osfingerprints":null},"distance":{"value":0},"uptime":{"seconds":0,"lastboot":""},"tcpsequence":{"index":0,"difficulty":"","vaules":""},"ipidsequence":{"class":"","values":""},"tcptssequence":{"class":"","values":""},"hostscripts":null,"trace":{"proto":"","port":0,"hops":null},"times":{"srtt":"","rttv":"","to":""}}}`
 				So(j, ShouldEqual, expectedJSON)
 			})
 		})
