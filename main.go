d648e9e2c9df9bcbf5b032487f2d0a1f4e63ce6d
Alessio Sanfratello <sanfra90@gmail.com>
2021-04-28 23:08:29 +0200

    removed useless signal handler

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"flag"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/alesanfra/ground-control/agent"
@@ -18,9 +15,6 @@ func main() {
 	port := flag.Uint("p", 3000, "HTTP port")
 	flag.Parse()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
 	devices := scanner.NewDeviceMap()
 
 	services := []agent.Service{
@@ -29,12 +23,9 @@ func main() {
 		web.NewWebServer(devices, *port),
 	}
 
-	if err := agent.Run(ctx, services); err != nil {
+	if err := agent.Run(context.Background(), services); err != nil {
 		log.Fatalf("Error on agent run: %v", err)
 	}
 
-	c := make(chan os.Signal)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-	<-c
 	log.Print("Shutdown")
 }
